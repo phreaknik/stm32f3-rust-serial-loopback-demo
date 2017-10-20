216cded271d6707db149e5fb892e1545266defff
phreaknik <johnboydiv@gmail.com>
2017-10-19 21:44:41 -0500

    Copied serial loopback example from f3 crate

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,53 @@
 #![feature(const_unsafe_cell_new)]
 #![no_std]
 
+extern crate cortex_m_rtfm as rtfm;
 extern crate f3;
-extern crate cortex_m_rtfm as rtfm; // <- this rename is required
-extern crate cortex_m_semihosting as semihosting;
 
-use core::fmt::Write;
-use rtfm::app; // <- this is a procedural macro
-use semihosting::hio;
+use f3::prelude::*;
+use f3::Serial;
+use f3::serial::Event;
+use f3::time::Hertz;
+use rtfm::{app, Threshold};
 
-// This macro expands into the `main` function
+const BAUD_RATE: Hertz = Hertz(115_200);
+
+// TASKS & RESOURCES
 app! {
-    // this is a path to a _device_ crate, a crate generated using svd2rust
     device: f3::stm32f30x,
+
+    tasks: {
+        USART1_EXTI25: {
+            path: loopback,
+            resources: [USART1],
+        },
+    }
 }
 
 // INITIALIZATION
-fn init(_p: init::Peripherals) {
-    // Nothing to initialize in this example ...
+fn init(p: init::Peripherals) {
+    let serial = Serial(p.USART1);
+
+    serial.init(BAUD_RATE.invert(), Some(p.DMA1), p.GPIOA, p.RCC);
+    serial.listen(Event::Rxne);
 }
 
 // IDLE LOOP
 fn idle() -> ! {
-    writeln!(hio::hstdout().unwrap(), "Hello, world!").unwrap();
-
-    // Go to sleep
+    // Sleep
     loop {
         rtfm::wfi();
     }
-} 
+}
+
+// TASKS
+// Send back the received byte
+fn loopback(_t: &mut Threshold, r: USART1_EXTI25::Resources) {
+    let serial = Serial(&**r.USART1);
+
+    // Grab the byte we just received
+    let byte = serial.read().unwrap();
+
+    // and send it back
+    serial.write(byte).unwrap();
+}
